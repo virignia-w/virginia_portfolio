17b20b5e9a4247ebface604219b74ba07d31b91d
virginia <ning93315@gmail.com>
2025-01-17 20:22:24 -0500

    Fix nav bar

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image"
 import logo from "../public/LOGO.png"
@@ -19,30 +21,22 @@ export default function Header() {
 
 
     return <>
-        <nav className="navbar navbar-expand-sm mb-0">
+        <nav className="navbar navbar-expand-lg mb-0">
             <div className="container-fluid">
-                <div>
-                    <a className="navbar-brand" href="/home"> <Image className="logo" src={logo} style={{ width: 70, height: 45 }} alt="logo" /> </a>
-                </div>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" id="linkbar" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="/home"> 
+                <Image className="logo" src={logo} style={{ width: 70, height: 45 }} alt="logo" /> 
+                </a>
+                <button className="navbar-toggler collapseB" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="nav " >
-                        <li className="nav-item">
-                            <a className="nav-link" href="/home">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/project">Project</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/about">About Me</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/about#contact">Contact</a>
-                        </li>
-                    </ul>
+                <div className="collapse navbar-collapse navC" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link nav-link active" aria-current="page" href="/home">Home</a>
+                        <a className="nav-link nav-link active" aria-current="page" href="/project">Project</a>
+                        <a className="nav-link nav-link active" aria-current="page" href="/about">About Me</a>
+                        <a className="nav-link nav-link active" aria-current="page" href="/about#contact">Contact</a>
+                    </div>
                 </div>
             </div>
         </nav>
